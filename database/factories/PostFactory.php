975e8bc56be7a38a6c74cb38478f97a017c0da53
nkara <nkaragodov@inbox.ru>
2023-11-02 20:58:58 +0300

    Создание База данных, миграций и модели. Фабрики и сиды

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->name(),
+            'preview' => $this->faker->text(),
+            'description' => $this->faker->text(50),
+            "thumbnail" => $this->faker->loremflickr('posts'),
+        ];
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+        <title>Laravel c нуля</title>
+    </head>
+    <body class="antialiased">
+        <h1>Laravel c нуля</h1>
+        <script src="/js/app.js"></script>
+    </body>
+</html>
