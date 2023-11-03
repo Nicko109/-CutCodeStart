36dee4b31f55dc9b6493ceaf39da49303138fd11
nkaragodov@inbox.ru <Nevermore109>
2023-11-03 15:14:35 +0300

    Настроена маршрутизация и выведены index и show

--- config/filesystems.php
+++ config/filesystems.php
@@ -33,11 +33,10 @@ return [
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
-            'throw' => false,
         ],
 
         'public' => [
-            'driver' => 'public',
+            'driver' => 'local',
             'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
